4ac4e139d603e86bc9e81d25f6db8588b98afc73
Christopher Thai <chris98.vthai@sbcglobal.net>
2018-05-10 18:36:13 -0700

    Initial Heroku setup

--- commands/catpic.js
+++ commands/catpic.js
@@ -3,40 +3,12 @@ module.exports = {
 	description: 'You\'re bored and you want a cat pic',
 	usage: '!catpic',
 	execute(message, args) {
-		var num = Math.floor(Math.random() * 10);
-		var filename;
-		switch (num) {
-			case 0:
-				filename = './pics/catpic0.jpg';
-				break;
-			case 1:
-				filename = './pics/catpic1.jpg';
-				break;
-			case 2:
-				filename = './pics/catpic2.jpg';
-				break;
-			case 3:
-				filename = './pics/catpic3.jpg';
-				break;
-			case 4:
-				filename = './pics/catpic4.jpg';
-				break;
-			case 5:
-				filename = './pics/catpic5.jpg';
-				break;
-			case 6:
-				filename = './pics/catpic6.jpg';
-				break;	
-			case 7:
-				filename = './pics/catpic7.webp';
-				break;
-			case 8:
-				filename = './pics/catpic8.webp';
-				break;
-			case 9:
-				filename = './pics/catpic9.webp';
-				break;
-		}
-		message.channel.send('Here you go! :cat:', {files: [filename]});
+		var fs = require('fs');
+		var files = fs.readdirSync('pics/');
+
+		var num = Math.floor(Math.random() * files.length);
+		var filename = files[num];
+	
+		message.channel.send('Here you go! :cat:', {files: [`pics/${filename}`]});
 	}
 }
